492852cd06f5717f667f99c56dd43dab7682a007
AkshayCdr <akshaychandra418@gmail.com>
2024-04-24 10:19:59 +0530

    added components

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,6 @@
+
+export default function Button() {
+  return (
+    <button type="submit" name="">Send</button>
+  )
+}
---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import Input from "./Input";
+
+export default function Chat() {
+  return (
+    <div className="chat-container">
+      <div className="chat-box"></div>
+      <Input />
+      <Button />
+    </div>
+  );
+}
