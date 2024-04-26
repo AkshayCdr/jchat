ae55989530e840d69501066d48a9cbad597a7615
AkshayCdr <akshaychandra418@gmail.com>
2024-04-26 15:41:47 +0530

    temp

--- server/index.js
+++ server/index.js
@@ -38,6 +38,26 @@ io.on("connection", (socket) => {
       });
     // console.log(users);
     io.emit("get-users", users);
+
+    // const targetSocket = io.sockets.sockets.get(socket.id); // Get the socket object by ID
+    // if (targetSocket) {
+    //   targetSocket.emit("private", {
+    //     id: socket.id,
+    //     msg: "secrete message",
+    //   });
+    // } else {
+    //   console.log("Socket not found or disconnected.");
+    // }
+
+    io.to(socket.id).emit("private", {
+      id: socket.id,
+      msg: "secreate message",
+    });
+
+    // io.emit("private", {
+    //   id: socket.id,
+    //   msg: "secreate message",
+    // });
   });
 
   // socket.on("join", function (data) {
@@ -52,16 +72,19 @@ io.on("connection", (socket) => {
     const username = data.username;
     const userNameToSend = data.user;
     const user = users.find((user) => user.userId === userNameToSend);
-    console.log(users.find((user) => user.userId === userNameToSend));
-    console.log(user);
+    // console.log(users.find((user) => user.userId === userNameToSend));
+    // console.log(user);
+    console.log({ data, users, user });
+
     // console.log(user.socketId);
     // console.log(message);
     // if (user) io.to(user.socketId).emit("receive-message", message);
-    if (user)
-      io.to(user.socketId).emit("receive-message", [{ username, message }]);
+    // if (user)
+    //   io.to(user.socketId).emit("receive-message", [{ username, message }]);
+
     // io.sockets.in("user1@example.com").emit("receive-message", message);
-    // io.emit("receive-message", [username + ":" + message]);
-    // io.emit("receive-message", [{ username, message }]);
+
+    io.emit("receive-message", [{ username, message }]);
   });
   // socket.on("chat-message", (message) => {
   //   console.log(message);
